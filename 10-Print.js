f549e9b35c4d18ee0b0fd5f6f82b9e082fbcc627
Kyle <31635647+KylePeters05@users.noreply.github.com>
2017-10-04 09:52:18 -0400

    Create 10-Print.js

---
+++ 10-Print.js
@@ -0,0 +1,25 @@
+let x = 0;
+let y = 0;
+let spacing = 5;
+
+function setup() {
+
+  createCanvas(400,400);
+  background(0);
+
+}
+
+function draw() {
+  stroke(225);
+  if (random(1) < 0.5) {
+    line(x , y , x + 10, y + spacing);
+  }
+  else {
+    line(x, y + spacing, x + spacing, y);
+  }
+  x = x + spacing;
+  if (x > width) {
+    x = 0;
+    y = y + spacing;
+  }
+}
